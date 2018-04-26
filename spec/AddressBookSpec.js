fc9d9e46f2919cdba3875c6289d2755d6d7f43c9
Abhishek Sah <abhi.sah.97@gmail.com>
2018-04-26 20:33:21 +0530

    To test Asynchronous Methods

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,21 +1,38 @@
 //A new Suite described called address book 
 //and within it we have a spec called "Should be able to add a contact"
-
 describe('Address Book' ,function () {
+	var addressBook,
+		thisContact;
+
+	beforeEach(function(){
+		addressBook = new AddressBook(),
+		thisContact=new Contact();	
+	});
+
 	it('Should be able to add a contact', function(){
-		var addressBook = new AddressBook(),
-			thisContact=new Contact();
 
 		addressBook.addContact(thisContact);			//testing the addContact function by getContact feature
-
 		expect(addressBook.getContact(0)).toBe(thisContact);
 	});
-	it('Should be able to delete a contact', function(){
-		var addressBook = new AddressBook(),
-			thisContact=new Contact();
 
+	it('Should be able to delete a contact', function(){
 		addressBook.addContact(thisContact);
 		addressBook.deleteContact(0);
 		expect(addressBook.getContact(0)).not.toBeDefined();
 	});
-} );
+});
+
+describe('Async Address Book', function(){
+	var addressBook= new AddressBook();
+	
+	beforeEach(function(done) {
+		addressBook.getInitialContacts(function(){
+			done();
+		});
+	});
+
+	it('Should grab initial contacts', function(done){
+		expect(addressBook.initialComplete).toBe(true);
+		done();
+	});
+});
